705cc8e24d1880b22b96d5fe31e9ae4f36c10196
Veselin <veselin.martinov@gmail.com>
2025-04-11 20:14:52 +0200

    add requirements

--- convert_scripts/pdf_to_word.py
+++ convert_scripts/pdf_to_word.py
@@ -1,23 +1,30 @@
 import os
 from pdf2docx import Converter
 
-def pdf_to_word(pdf_path, word_path=None):
-    if not os.path.exists(pdf_path):
-        print('File not found')
+def pdf_to_doc(input_dir, output_dir):
+    if not os.path.exists(input_dir):
+        print('Directory not found')
         return
 
-    if word_path is None:
-        word_path = os.path.splitext(pdf_path)[0] + '.docx'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    try:
-        cv = Converter(pdf_path)
-        cv.convert(word_path, start=0, end=None)
-        cv.close()
-        print('Conversion successful')
-    except Exception as e:
-        print(f"Error: {e}")
+    for file_name in os.listdir(input_dir):
+        if file_name.lower().endswith('.pdf'):
+            pdf_path = os.path.join(input_dir, file_name)
+            docx_filename = os.path.splitext(file_name)[0] + '.docx'
+            docx_file_path = os.path.join(output_dir, docx_filename)
+
+            try:
+                cv = Converter(pdf_path)
+                cv.convert(docx_file_path, start=0, end=None)
+                cv.close()
+                print('Conversion successful')
+            except Exception as e:
+                print(f"Error: {e}")
 
 if __name__ == '__main__':
-    pdf_path = input('PDF path:')
-    pdf_to_word(pdf_path)
+    input_path = '/home/veselin/pdf_t_w'
+    output_path = '/home/veselin/pdf_t_w/docs'
+    pdf_to_doc(input_path, output_path)
 
--- convert_scripts/word_to_pdf.py
+++ convert_scripts/word_to_pdf.py
@@ -1,23 +1,27 @@
 import os
 import subprocess
 
-def word_to_pdf(word_path, pdf_path=None):
-    if not os.path.exists(word_path):
-        print('File not found')
+def doc_to_pdf(input_dir, output_dir):
+    if not os.path.exists(input_dir):
+        print('Input directory not found!')
         return
 
-    if pdf_path is None:
-        pdf_path = os.path.splitext(word_path)[0] + '.pdf'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    try:
-        subprocess.run(
-            ["soffice", "--headless", "--convert-to", "pdf", word_path, "--outdir", os.path.dirname(pdf_path)],
-            check=True
-        )
-        print(f"Conversion successful: {pdf_path}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+    for file_name in os.listdir(input_dir):
+        word_path = os.path.join(input_dir, file_name)
+
+        try:
+            subprocess.run(
+                ["soffice", "--headless", "--convert-to", "pdf", word_path, "--outdir", output_dir],
+                check=True
+            )
+            print(f"Conversion successful: {os.path.splitext(file_name)[0] + '.pdf'}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
 
 if __name__ == '__main__':
-    word_path = input('Word path:')
-    word_to_pdf(word_path)
+    input_path = '/home/veselin/w_t_pdf'
+    output_path = '/home/veselin/w_t_pdf/pdfs'
+    doc_to_pdf(input_path, output_path)
